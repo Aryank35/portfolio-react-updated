c0692577f1e9b6a9c05ff99c16bf0b530abd879d
Aryank35 <aryan5935kishan@gmail.com>
2023-10-16 01:20:39 +0530

    Education section

--- src/Components/Education/Education.jsx
+++ src/Components/Education/Education.jsx
@@ -1,26 +1,30 @@
 import React from 'react'
+import data from "../../utills/EduInfo"
+import Card from './Card'
 
 const Education = () => {
   return (
-    
-    <div className='Education bg-purple-950 w-full items-center py-4 mx-auto  md:px-[20%] 2xl:px-[35%]'>
+    <div className='Education bg-[#1b1e2e] w-full items-center py-8 mx-auto  md:px-[20%] 2xl:px-[35%]'>
         <div className="heading py-5">
             <h1 className='text-5xl text-white font-bold text-center'>Education</h1>
         </div>
-        <div className="EduBox flex flex-col border-2 items-center  ">
-            <div className="box1">
-                <div className="college-info flex p-2" id='collegeInfo'>
-                    <img src="./gita.png" className='h-[50px] md:h-[80px]' alt="" />
-                    <div className="college-details text-white flex flex-col pl-3">
-                    <h1 className="collegeName font-semibold md:text-lg text-white ">Gandhi Institute Of Technology and Advancement(GITA)</h1>
-                    <p className="course text-slate-300 text-sm">Bachelor of Technology - BTech, Computer Science and Engineering</p>
-                    <p className='text-gray-300 text-sm'>2021-25</p>
-                    </div>
-                </div>
-                <div className="college-desc md:text-xl text-white hidden " id='collegeDesc'>
-                I am currently pursuing a Bachelor's degree in Computer Science and Engineering at GITA, Bhubaneswar. I have completed 4 semesters and have a CGPA of 9.3. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am learning and working on exciting projects with a team of talented developers.
-                </div>
-            </div>
+        <div className="EduBox space-y-9 flex flex-col justify-center items-center">
+            {
+                data.map((card)=>{
+                    return(
+                        <Card 
+                            img={card.collegeLogo}
+                            collegeName={card.collegeName}
+                            course={card.course}
+                            period={card.period}
+                            details={card.details}
+                        />
+                    )
+                })
+            }
+            
+            
+            
         </div>
     </div>
   )
---
+++ src/utills/EduInfo.js
@@ -0,0 +1,26 @@
+const data =[
+    {
+        collegeLogo: "./gita.png",
+        collegeName: "Gandhi Institute Of Technology and Advancement(GITA)",
+        course: "Bachelor of Technology - BTech, Computer Science and Engineering",
+        period: "2021-25",
+        details: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at GITA, Bhubaneswar. I have completed 4 semesters and have a CGPA of 9.3. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am learning and working on exciting projects with a team of talented developers."
+    },
+    {
+        collegeLogo: "./ssb.png",
+        collegeName: "Sainik School Bhubaneswar",
+        course: "CBSE(XII), Science with Computer",
+        period: "2020-21",
+        details: "During my time at Sainik School Bhubaneswar, I was exposed to a comprehensive curriculum that fostered academic excellence, character development, and leadership skills. The school's rigorous academic program, combined with a strong emphasis on discipline and values, laid a solid foundation for my future endeavors."
+    },
+    {
+        collegeLogo: "./ssb.png",
+        collegeName: "Sainik School Bhubaneswar",
+        course: "CBSE(X), Science with Computer",
+        period: "2018-19",
+        details: "During my time at Sainik School Bhubaneswar, I was exposed to a comprehensive curriculum that fostered academic excellence, character development, and leadership skills. The school's rigorous academic program, combined with a strong emphasis on discipline and values, laid a solid foundation for my future endeavors."
+    }
+]
+
+
+export default data;
