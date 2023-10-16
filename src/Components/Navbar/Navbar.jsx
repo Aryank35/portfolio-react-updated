8222eecd4c057ee4d040e40e9eecde57d9eef2ad
Aryank35 <aryan5935kishan@gmail.com>
2023-10-16 19:12:58 +0530

    navbar pc view updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -12,22 +12,29 @@ const Navbar = () => {
   };
 
   return (
-    <nav className='Navbar bg-[#040015] py-2 flex items-center justify-between fixed w-[100%] h-[60px] top-0'>
-      <div className='flex  my-auto ml-4 sm:ml-6 pt-2'>
+    <nav className='Navbar bg-[#040015] py-2  flex items-center justify-between fixed w-[100%] h-[55px] sm:h-[70px] '>
+      <div className='flex relative -top-1  my-auto ml-4 sm:ml-6 pt-2'>
         <a href="#" className='flex space-x-1 items-center'>
             <img src="./logo.jpg" alt="linkedin icon" className='sm:h-[40px] h-[35px]  bg-center rounded-full' />
             <h1 className='sm:text-2xl text-xl font-bold text-white flex text-center'>Portfolio</h1>
         </a>
         
         </div>
-        <div className=' hidden sm:flex mb-4 '>
+        <div className=' hidden sm:flex mb-4 space-x-3 relative top-3 items-center mx-8'>
           <ul className='flex justify-center sm:text-lg sm:space-x-2 sm:font-semibold md:text-xl md:space-x-10 md:font-bold text-white'>
             <li><a className='hover:text-purple-400' href="#hero">About</a></li>
             <li><a className='hover:text-purple-400' href="#skills">Skills</a></li>
             <li><a className='hover:text-purple-400' href="#project">Project</a></li>
             <li><a className='hover:text-purple-400' href="#education">Education</a></li>
+            <li><a className='hover:text-purple-400' href="#education">Contact me</a></li>
           </ul>
+          
         </div>
+        <div className='hidden sm:flex items-center relative top-2 mr-8'>
+            <a href="https://github.com/Aryank35" target='_blank' className='mx-auto px-2 flex justify-center font-bold border-purple-200 border-2 rounded-2xl text-purple-400 hover:bg-purple-400 hover:text-black hover:transition-all hover:ease-in-out hover:duration-700 hover:delay-75 mb-4'>
+              <button className='px-1 py-2 text-lg text-center'>Github profile</button>
+            </a>
+          </div>
       <div className='MenuBar flex sm:hidden items-center my-auto pt-2' id='MenuBar' onClick={toggleMobileMenu}>
         <img src="./menu.png" alt="Menu Icon" className='h-[30px] mb-8 mr-4 transition-all delay-300 ease-in-out cursor-pointer' />
       </div>
@@ -42,6 +49,7 @@ const Navbar = () => {
           <li><a className='hover:text-purple-400 active:underline py-1' href="#skills" onClick={toggleMobileMenu}>Skills</a></li>
           <li><a className='hover:text-purple-400 active:underline py-1' href="#project" onClick={toggleMobileMenu}>Project</a></li>
           <li><a className='hover:text-purple-400 active:underline py-1' href="#education" onClick={toggleMobileMenu}>Education</a></li>
+          <li><a className='hover:text-purple-400 active:underline py-1' href="#education" onClick={toggleMobileMenu}>Contact me</a></li>
           <div>
             <a href="https://github.com/Aryank35" target='_blank' className='mx-auto px-2 flex justify-center font-bold border-purple-200 border-2 rounded-2xl text-purple-400 hover:bg-purple-400 hover:text-black hover:transition-all hover:ease-in-out hover:duration-700 hover:delay-75 mb-4'>
               <button className='px-1 py-2 text-lg text-center'>Github</button>
